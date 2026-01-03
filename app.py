71aa6c2b5f61c8a7cac01ebbdff709ec3ba9b413
ShrutiPatel263 <shrutipatel1126@gmail.com>
2026-01-03 23:05:44 +0530

    DataBase Created

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskTopics.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
+
+class Todo(db.column):
+    srNo=db.comumn(db.integer,primary_key=True)
+    title=db.comumn(db.string(200),nullable=False)
+    desc=db.comumn(db.string(500),nullable=False)
+    data_created=db.column(db.DateTime,default=datetime.utcnow)
 
 @app.route("/")
 def hello_world():
